51cd233e5a63b71a97d13677c47e389262f7ca03
jyothi s <jyothisannapuri@gmail.com>
2022-02-26 11:57:33 +0530

    Add project files.

---
+++ Actionfiltersdemo/Controllers/CountriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Actionfiltersdemo.Controllers
+{
+    public class CountriesController : Controller
+    {
+        // GET: Countries
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Countries(List<string> countrynames)
+        {
+            return View(countrynames);
+        }
+    }
+}
---
+++ Actionfiltersdemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Actionfiltersdemo.MvcApplication" Language="C#" %>
